1c771b09ec8d91431350050679a1f209ac80cf38
Vaibhav Arora <aroravaibhav817@gmail.com>
2026-03-02 22:02:56 +0530

    feat: add writer

---
+++ src/io/writer.rs
@@ -0,0 +1,26 @@
+use crate::error::AppError;
+use crate::models::account::Account;
+use std::io::Write;
+
+pub fn write_accounts<W: Write>(
+    writer: W,
+    accounts: impl Iterator<Item = Account>,
+) -> Result<(), AppError> {
+    let mut wtr = csv::Writer::from_writer(writer);
+
+    // write header manually for clarity
+    wtr.write_record(&["client", "available", "held", "total", "locked"])?;
+
+    for account in accounts {
+        wtr.write_record(&[
+            account.client.to_string(),
+            account.available.to_string(),
+            account.held.to_string(),
+            account.total()?.to_string(),
+            account.locked.to_string(),
+        ])?;
+    }
+
+    wtr.flush()?;
+    Ok(())
+}
